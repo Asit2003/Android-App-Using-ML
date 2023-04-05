e94d826d43fde3ee91e9efe96b3dd49718abf6c6
Asit Ghosh <asitg2003@gmail.com>
2023-04-06 00:00:41 +0530

    Login page, signup page, forgot password page

--- app/src/main/java/com/example/xss/MainActivity.java
+++ app/src/main/java/com/example/xss/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.xss;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        button=findViewById(R.id.logout);
+        textView=findViewById(R.id.user_details);
+        user=auth.getCurrentUser();
+        if(user==null){
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivities(new Intent[]{intent});
+            finish();
+        }
+        else{
+            textView.setText(user.getEmail());
+        }
+        gso =new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        gsc = GoogleSignIn.getClient(this,gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null){
+            String Name = account.getDisplayName();
+            String Mail = account.getEmail();
+
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivities(new Intent[]{intent});
+                finish();
+            }
+        });
     }
 }
