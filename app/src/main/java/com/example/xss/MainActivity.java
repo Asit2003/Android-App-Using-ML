dbf5adb9e0e1c60787e436121cca6120fe9010fd
Asit Ghosh <asitg2003@gmail.com>
2023-04-10 19:58:45 +0530

    ML tool page and Main activity page ... by Asit and Anshit

--- app/src/main/java/com/example/xss/MainActivity.java
+++ app/src/main/java/com/example/xss/MainActivity.java
@@ -1,62 +1,100 @@
 package com.example.xss;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
-    GoogleSignInOptions gso;
-    GoogleSignInClient gsc;
+
+    private Button mlModelButton;
+    private Button settingsButton;
+    private Button profileButton;
+    private Button historyButton;
+    private Button logoutButton;
+
+    private GoogleSignInClient googleSignInClient;
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        auth = FirebaseAuth.getInstance();
-        button=findViewById(R.id.logout);
-        textView=findViewById(R.id.user_details);
-        user=auth.getCurrentUser();
-        if(user==null){
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivities(new Intent[]{intent});
-            finish();
-        }
-        else{
-            textView.setText(user.getEmail());
-        }
-        gso =new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        // Set up Google Sign In options
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
-        gsc = GoogleSignIn.getClient(this,gso);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if(account!=null){
-            String Name = account.getDisplayName();
-            String Mail = account.getEmail();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
 
-        }
-        button.setOnClickListener(new View.OnClickListener() {
+        // Set up Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        mlModelButton = findViewById(R.id.model_button);
+        settingsButton = findViewById(R.id.settings_button);
+        profileButton = findViewById(R.id.profile_button);
+        historyButton = findViewById(R.id.history_button);
+        logoutButton = findViewById(R.id.logout_button);
+
+        mlModelButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivities(new Intent[]{intent});
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ModelActivity.class));
+            }
+        });
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ModelActivity.class));
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ModelActivity.class));
+            }
+        });
+
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ModelActivity.class));
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Sign out from Google and Firebase
+                googleSignInClient.signOut();
+                firebaseAuth.signOut();
+
+                // Redirect to the login activity
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
                 finish();
             }
         });
+
+        // Check if the user is already signed in
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account == null) {
+            // Redirect to the login activity
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
